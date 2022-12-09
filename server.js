d2bf38f13d014b484a6b5be96340326cba33db5c
webdragon0125 <jakus.superdev@gmail.com>
2022-12-09 13:57:02 +0900

    vercel.json added

--- server.js
+++ server.js
@@ -15,48 +15,48 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
 app.use(bodyParser.json({limit: "50mb", extended: true}))
 
 app.get('/', (req, res) => {
-    res.send('<h1>R</h1>');
+    res.send('Hey this is my API running 🥳')
 });
 
 app.post('/send_mail', (req, res) => {
-    // var url = req.body.data_url;
-    // var url1 = url.split("base64,")[1];
-    // var attach = new Array();
-    // for (var i = 0; i < req.body.images.length; i++) {
-    //   var data_url = req.body.images[i].data_url.split('base64')[1];
-    //   attach.push({
-    //     filename: req.body.images[i].name,
-    //     content: data_url,
-    //     encoding: 'base64'
-    //   })
-    // }
-    var transport = nodemailer.createTransport(
-      smtpTransport({
+// var url = req.body.data_url;
+// var url1 = url.split("base64,")[1];
+// var attach = new Array();
+// for (var i = 0; i < req.body.images.length; i++) {
+//   var data_url = req.body.images[i].data_url.split('base64')[1];
+//   attach.push({
+//     filename: req.body.images[i].name,
+//     content: data_url,
+//     encoding: 'base64'
+//   })
+// }
+var transport = nodemailer.createTransport(
+    smtpTransport({
         service: "Gmail",
         auth: {
-          user: "jakus.superdev@gmail.com",
-          pass: "fjxxdvcppecaaqtu",
+            user: "jakus.superdev@gmail.com",
+            pass: "fjxxdvcppecaaqtu",
         },
-      })
-    );
-  
-    // setup e-mail data with unicode symbols
-    var mailOptions = {
-      from: 'jakus.superdev@gmail.com', // sender address
-      to: "jakus.superdev@gmail.com", // list of receivers
-      subject: "User sent msg.", // Subject line
-      text: "User sent msg.", // plaintext body
-      html: "<html><body><p>Name: " + req.body.name + "</p><p>Email:" + req.body.email + "</p><p>Message:" + req.body.msg + "</p></body></html>",
-    //   attachments: attach
-    };
-    // send mail with defined transport object
-    transport.sendMail(mailOptions, function (error, response) {
-      if (error) {
-        res.send({'error': 'Something wrong!'});
-        console.log(error);
-      } else {
-        console.log('Suceess');
-        res.status(200).json({success: 'Hello'});
-      }
-    });
-  })
+    })
+);
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'jakus.superdev@gmail.com', // sender address
+    to: "jakus.superdev@gmail.com", // list of receivers
+    subject: "User sent msg.", // Subject line
+    text: "User sent msg.", // plaintext body
+    html: "<html><body><p>Name: " + req.body.name + "</p><p>Email:" + req.body.email + "</p><p>Message:" + req.body.msg + "</p></body></html>",
+//   attachments: attach
+};
+// send mail with defined transport object
+transport.sendMail(mailOptions, function (error, response) {
+    if (error) {
+    res.send({'error': 'Something wrong!'});
+    console.log(error);
+    } else {
+    console.log('Suceess');
+    res.status(200).json({success: 'Hello'});
+    }
+});
+})
